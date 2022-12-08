3829ba4d09749fac7bd484e18f0f0560581a6d86
dkengineering0 <dmytro.kyryienko.engineer@gmail.com>
2022-12-08 09:32:33 +0300

    8

--- solutions/8/first.rb
+++ solutions/8/first.rb
@@ -3,7 +3,46 @@
 module Day8
   class Part1 < BaseSolution
     def run
-      'not ready'
+      @found = Set.new
+
+      data = chomped_lines.each_with_index.map do |line, i|
+        line.chars.map(&:to_i).each_with_index.map do |h, j|
+          [h, [i, j]]
+        end
+      end
+
+      slice_rows(data).each do |row|
+        find_from_left(row)
+        find_from_left(row.reverse)
+      end
+
+      slice_columns(data).each do |row|
+        find_from_left(row)
+        find_from_left(row.reverse)
+      end
+
+      @found.count
+    end
+
+    def self.expected_result()
+      21
+    end
+
+    def slice_rows(data)
+      data
+    end
+
+    def slice_columns(data)
+      data.transpose
+    end
+
+    def find_from_left(data)
+      @found.add(data.first.last)
+      max = data.first.first
+      data.each_cons(2) do |l, r|
+        @found.add(r.last) if r.first > max
+        max = [r.first, max].max
+      end
     end
   end
 end
--- solutions/8/second.rb
+++ solutions/8/second.rb
@@ -3,7 +3,42 @@
 module Day8
   class Part2 < Part1
     def run
-      'not ready'
+      @data = chomped_lines.map do |line|
+        line.chars.map(&:to_i)
+      end
+
+      scenic_scores = (0...@data.size).to_a.flat_map do |i|
+        (0...@data.first.size).to_a.map do |j|
+          directions.reduce(1) do |acc, dir|
+            acc * scenic_score([i, j], dir)
+          end
+        end
+      end
+
+      scenic_scores.max
+    end
+
+    def scenic_score(from, dir)
+      i, j = *from
+      di, dj = *dir
+      result = 0
+      while next_tree = safe_dig(i += di, j += dj)
+        result += 1
+        break if next_tree >= @data[from.first][from.last]
+      end
+      result
+    end
+
+    def directions
+      [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    end
+
+    def safe_dig(i, j)
+      @data.dig(i, j) if i >= 0 && j >= 0
+    end
+
+    def self.expected_result
+      8
     end
   end
 end
