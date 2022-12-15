744312c47290c15616d67e1ea7df8766169c5424
dkengineering0 <dmytro.kyryienko.engineer@gmail.com>
2022-12-15 14:30:32 +0300

    15

--- solutions/15/first.rb
+++ solutions/15/first.rb
@@ -3,7 +3,37 @@
 module Day15
   class Part1 < BaseSolution
     def run
-      'not ready'
+      grid = chomped_lines.map do |line|
+        sx, sy, bx, by = line.scan(/-?\d+/).map(&:to_i)
+        [sx, sy, manhattan(sx, sy, bx, by)]
+      end
+
+      xl = Float::INFINITY
+      xh = -Float::INFINITY
+
+      grid.each do |sx, sy, sc|
+        dx = sc - (target_row - sy).abs
+
+        next unless dx > 0
+
+        xl = [xl, sx - dx].min
+        xh = [xh, sx + dx].max
+      end
+
+      xh - xl
+    end
+
+    def manhattan(x1, y1, x2, y2)
+      (x1-x2).abs + (y1-y2).abs
+    end
+
+    def target_row
+      10 # for test input
+      # 2_000_000 # for real input
+    end
+
+    def self.expected_result
+      26
     end
   end
 end
--- solutions/15/second.rb
+++ solutions/15/second.rb
@@ -1,9 +1,40 @@
 # frozen_string_literal: true
 
 module Day15
-  class Part2
+  class Part2 < BaseSolution
     def run
-      'not ready'
+      grid = chomped_lines.map do |line|
+        sx, sy, bx, by = line.scan(/-?\d+/).map(&:to_i)
+        [sx, sy, manhattan(sx, sy, bx, by)]
+      end
+
+      grid.each do |sx, sy, sc|
+        (0..sc).each do |pt|
+          [
+            [sx - sc - 1 + pt, sy - pt],
+            [sx + sc + 1 - pt, sy - pt],
+            [sx - sc - 1 + pt, sy + pt],
+            [sx + sc + 1 - pt, sy + pt]
+          ].each do |tx, ty|
+            next unless (0..target_range) === tx && (0..target_range) === ty
+            next unless grid.all? { |(ox, oy, od)| manhattan(tx, ty, ox, oy) > od }
+            return tx * 4000000 + ty
+          end
+        end
+      end
+    end
+
+    def manhattan(x1, y1, x2, y2)
+      (x1-x2).abs + (y1-y2).abs
+    end
+
+    def target_range
+      # 20 # for test input
+      4_000_000 # for real input
+    end
+
+    def self.expected_result
+      56000011
     end
   end
 end
